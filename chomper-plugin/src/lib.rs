d7e7ff916c2aabbc054d44da00a4d311a30283e7
klebs <none>
2022-08-11 01:13:19 -0400

    updates to rust-analyzer for plugin architecture

--- chomper-plugin/src/lib.rs
+++ chomper-plugin/src/lib.rs
@@ -23,7 +23,6 @@ pub struct KlebsFixBabyRustConfig {
 ///
 /// It is used for dynamically loading plugins.
 ///
-/// See `bevy_dynamic_plugin/src/loader.rs#dynamically_load_plugin`.
 pub type CreateKlebsFixBabyRustPlugin = unsafe fn() -> *mut dyn KlebsFixBabyRustPlugin;
 
 pub trait LoadKlebsFixBabyRustDynamicPlugin {
@@ -31,23 +30,13 @@ pub trait LoadKlebsFixBabyRustDynamicPlugin {
     fn load_klebs_fix_baby_rust_plugin(&self, path: &str);
 }
 
-/// A collection of Bevy app logic and
-/// configuration.
-///
-/// Plugins configure an [`App`]. When an [`App`]
-/// registers a plugin, the plugin's
-/// [`Plugin::build`] function is run.
 pub trait KlebsFixBabyRustPlugin: Any + Send + Sync + Debug {
 
-    /// Configures the [`App`] to which this
-    /// plugin is added.
     fn klebs_fix_baby_rust(&self, 
         config: &KlebsFixBabyRustConfig,
         file:   &SourceFile,
         range:  TextRange) -> TextEdit;
 
-    /// Configures a name for the [`Plugin`] which
-    /// is primarily used for debugging.
     fn name(&self) -> &str {
         std::any::type_name::<Self>()
     }
@@ -57,17 +46,38 @@ pub trait KlebsFixBabyRustPlugin: Any + Send + Sync + Debug {
 ///
 /// The plugin must export the
 /// [CreateKlebsFixBabyRustPlugin] function.
+#[tracing::instrument]
 pub fn dynamically_load_klebs_fix_baby_rust_plugin(path: &str) 
--> (Library, Box<dyn KlebsFixBabyRustPlugin>) {
+-> Result<(Library, Box<dyn KlebsFixBabyRustPlugin>), String> {
 
     let lib = unsafe { Library::new(path).unwrap() };
 
+    tracing::debug!("created lib: {:#?}", lib);
+
     unsafe {
 
-        let func: Symbol<CreateKlebsFixBabyRustPlugin> = lib.get(b"_create_klebs_fix_baby_rust_plugin").unwrap();
+        let symname = b"create_klebs_fix_baby_rust_plugin";
 
-        let plugin = Box::from_raw(func());
+        match lib.get(symname) {
+            Ok(symbol) => {
+                let func: Symbol<CreateKlebsFixBabyRustPlugin> = symbol;
 
-        (lib, plugin)
+                let plugin = Box::from_raw(func());
+
+                tracing::debug!("created plugin: {:#?}", plugin);
+
+                Ok((lib, plugin))
+            },
+            Err(e) => {
+
+                Err(
+                    format!("error while getting {} from {}: {}", 
+                        String::from_utf8(symname.to_vec()).unwrap(), 
+                        path,
+                        e, 
+                    )
+                )
+            }
+        }
     }
 }
--- chomper2/src/lib.rs
+++ chomper2/src/lib.rs
@@ -8,12 +8,57 @@ use syntax::{
     TextRange
 };
 
-use text_edit::TextEdit;
 use chomper_plugin::*;
+use text_edit::TextEdit;
+use ctor::ctor;
 
-#[derive(Default)]
+use tracing_log::LogTracer;
+use log;
+
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
+use tracing_subscriber::fmt::writer::{BoxMakeWriter,MakeWriter};
+use std::sync::Arc;
+use std::path::Path;
+use std::io;
+use std::fs::File;
+use lazy_static::lazy_static;
+
+#[derive(Debug)]
 struct KlebsFix {
 
+}
+
+impl Default for KlebsFix {
+
+    fn default() -> Self {
+
+        Self { 
+
+        }
+    }
+}
+
+fn write_stupid_file(file: &str, data: Option<&str>) {
+
+    let msg = match data {
+        Some(data) => data.to_string(),
+        None => {
+            let val: i32 = match std::fs::read_to_string(file) {
+                Ok(s) => {
+                    match s.parse() {
+                        Ok(parsed) => parsed,
+                        Err(_)     => 0,
+                    }
+                },
+                Err(_) => 0,
+            };
+
+            format!{"{}",val + 1}
+        }
+    };
+
+    std::fs::write(file, msg).expect("Unable to write file");
 }
 
 impl KlebsFixBabyRustPlugin for KlebsFix {
@@ -23,10 +68,13 @@ impl KlebsFixBabyRustPlugin for KlebsFix {
     // chomper integrations!
     //
     fn klebs_fix_baby_rust(
+        &self,
         config: &KlebsFixBabyRustConfig,
         file: &SourceFile,
         range: TextRange,
     ) -> TextEdit {
+
+        write_stupid_file("/tmp/foo", None);
 
         tracing::debug!("klebs_fix_baby_rust");
         tracing::debug!("config: {:?}", config);
@@ -81,6 +129,54 @@ impl KlebsFixBabyRustPlugin for KlebsFix {
     }
 }
 
-pub fn create_klebs_fix_baby_rust_plugin() -> *mut dyn KlebsFidBabyRustPlugin {
-    Box::new(KlebsFix::default()).into_raw()
+#[ctor] fn setup_logging() {
+
+    let logpath = std::env::var("RA_PLUGIN_LOG_FILE").ok();
+    let logflag = std::env::var("RA_PLUGIN_LOG").ok();
+
+    let msg = format!{
+        "in function setup_logging with RA_PLUGIN_LOG_FILE={} and RA_PLUGIN_LOG={}",
+        logpath.as_ref().unwrap(),
+        logflag.as_ref().unwrap(),
+    };
+
+    write_stupid_file("/tmp/foo-setup-logging", Some(msg.as_str()));
+
+    let file = match logpath {
+        Some(p) => match File::create(p) {
+            Ok(f)  => Some(f),
+            Err(e) => None,
+        },
+        None    => None,
+    };
+
+    write_stupid_file("/tmp/foo-check-file", Some(format!("{:#?}", file).as_str()));
+
+    let writer = match file {
+        Some(file) => BoxMakeWriter::new(Arc::new(file)),
+        None       => BoxMakeWriter::new(io::stderr),
+    };
+
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        .with_env_filter(tracing_subscriber::EnvFilter::from_env("RA_PLUGIN_LOG"))
+        .with_writer(writer)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+
+    tracing::info!("test trace info from chomper2!");
+}
+
+#[no_mangle]
+pub extern "C" fn create_klebs_fix_baby_rust_plugin() -> *mut dyn KlebsFixBabyRustPlugin {
+
+    let mut b = Box::new(KlebsFix::default());
+
+    Box::<KlebsFix>::into_raw(b)
 }
